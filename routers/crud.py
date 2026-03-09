1436db6fa53a0f00fcbf98827cb0c3f1888d6a02
AarushSrivatsa <pitlaaarushsrivatsa@gmail.com>
2026-03-10 00:28:35 +0530

    fixed link

--- routers/crud.py
+++ routers/crud.py
@@ -27,7 +27,8 @@ class RuleUpdate(BaseModel):
     dm_message: str
 
 def extract_shortcode(url: str) -> str:
-    return url.rstrip("/").split("/")[-1].lower()
+    url = url.rstrip("/").split("?")[0] 
+    return url.split("/")[-1].lower()
 
 async def get_media_id(video_link: str) -> str:
 
